c95311f93150ce40a2b5f612594947c31abe3e70
Alex <alexandrethiry999@gmail.com>
2024-01-23 19:35:25 -0500

    feat: continued work on basic backend + simplified recommended.tsx file with conditional rendering for logo + added dev script in packages.json for client

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-// router.get('/users', (req, res) => {
-    
-// })
+router.get('/', (req, res) => {
+    res.send("WELCOME TO THE SERVER OF A.T. RECIPES")
+})
+
+router.get('/recipes', (req, res) => {
+    res.json({
+        "choco-cake": "url",
+        "plain-cake": "url",
+        "choco-ice-cream": "url",
+        "vanilla-cake": "url",
+        "mango-sorbet": "url",
+        "strawberry-sorbet": "url",
+        "pear-sorbet": "url",
+    })
+})
 
 module.exports = router
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
 import "./Header.css"
 
 const Header = () => {
-    const tabs = ["Popular", "Fast", "Simple", "Add your own", "Log-In", "Sign-Up"]
+    const tabs = ["Image","Popular", "Fast", "Simple", "Add your own", "Log-In", "Sign-Up"]
 
     return (
         <div className="headerContainer">
-            <a href="index.html"><img className="logo" src="src/images/Logos/AT Recipes-logos_transparent.png" alt="Cooking hat logo" /></a>
-            {tabs.map(tab => <button className={`headerBtn ${tab}`} key={tab}>{tab}</button>)}
+            {tabs.map(tab => tab == "Image" ? <a href="index.html"><img className="logo" src="src/images/Logos/AT Recipes-logos_transparent.png" alt="Cooking hat logo" /></a>:<button className={`headerBtn ${tab}`} key={tab}>{tab}</button>)}
         </div>
     );
 }
