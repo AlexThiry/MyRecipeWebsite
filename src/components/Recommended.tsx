071c63634da657168445d331151e15e5e5a339e6
Alex <alexandrethiry999@gmail.com>
2024-01-23 22:08:17 -0500

    fix: ts syntax error with recipe data which wasn't declared

--- src/components/Recommended.tsx
+++ src/components/Recommended.tsx
@@ -1,8 +1,12 @@
 import {useState, useEffect} from "react";
 import "./Recommended.css"
 
+interface RecommededData {
+    recipes?: string[];
+}
+
 const Recommended = () => {
-    const  [backendData, setBackendData] = useState([{}])
+    const  [backendData, setBackendData] = useState<RecommededData>({});
     
     useEffect(() => {
         fetch('/api/recipes').then(
@@ -19,7 +23,7 @@ const Recommended = () => {
             {(typeof backendData.recipes === 'undefined') ? (
                 <p>Loading...</p>
             ) : (
-                backendData.recipes.map((recipe: string[], i: number) => (
+                backendData.recipes.map((recipe, i) => (
                     <p key={i}>{recipe}</p>
                   ))
             )}
